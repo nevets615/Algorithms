f569370c6f26127bbe05019f8801fd8f6afcf804
nevets615 <surj316@yahoo.com>
2020-02-04 23:20:26 -0500

    working on eating cookies

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,21 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  options = [['rock'], ['paper'], ['scissors']]
+
+  def inner(n, arr=[[]], cache={}):
+        newArr = []
+        if n == 0:
+            return arr
+        else:
+            for opt in options:
+                for item in arr:
+                    newArr += [opt + item]
+        plays = inner(n-1, newArr)
+        cache[n] = plays
+        return plays
+
+  return inner(n)
 
 
 if __name__ == "__main__":
